6d8dd30c70c1abc5bf672f5cd6b2e7c543fbc030
YanyangChen <184536632@qq.com>
2023-05-03 12:01:10 +0800

    3 files modified, 8 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,7 +5,7 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
@@ -13,8 +13,15 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
-}
 
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
+  onNotify2() {
+    window.alert('You will be notified 2 when the product goes on sale');
+  }
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
